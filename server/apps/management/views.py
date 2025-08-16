1d2008101d152eda68b89bb4723467fe048c2059
farmerlin731 <69831413+farmerlin731@users.noreply.github.com>
2025-08-16 15:26:28 +0800

    First example of class-view.

--- server/apps/management/views.py
+++ server/apps/management/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 
 # Create your views here.
@@ -12,3 +13,14 @@ def hello(request):
         message = "Hello World by POST method"
 
     return Response({"message": message})
+
+
+class HiView(APIView):
+    def _customMsg(self, method):
+        return f"Hi Hi by {method} method! :)"
+
+    def get(self, request):
+        return Response({"message": self._customMsg("GET")})
+
+    def post(self, request):
+        return Response({"message": self._customMsg("POST")})
