afbde25172fcb5179b666aea96d2192b923156ce
farmerlin731 <69831413+farmerlin731@users.noreply.github.com>
2025-08-16 11:23:22 +0800

    Fix Bug: 'apps'...

--- server/settings.py
+++ server/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "server.app.management",
+    "server.apps.management",
 ]
 
 MIDDLEWARE = [
