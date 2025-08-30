5e9251a3d6579582d4ce66862f3e65fc6d9cf2ad
farmerlin731 <69831413+farmerlin731@users.noreply.github.com>
2025-08-30 13:36:09 +0800

    Add serializer for returning item-list.

--- server/apps/playground/views.py
+++ server/apps/playground/views.py
@@ -2,6 +2,9 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from server.apps.playground.models import Item
+from server.apps.playground.serializers import ItemSerializer
+
 
 # Create your views here.
 @api_view(["GET", "POST"])
@@ -27,4 +30,6 @@ class HiView(APIView):
 
 class ItemListView(APIView):
     def get(self, request):
-        return Response({"message": "oh u got it"})
+        items = Item.objects.all()
+        serializer = ItemSerializer(items, many=True)
+        return Response(serializer.data)
